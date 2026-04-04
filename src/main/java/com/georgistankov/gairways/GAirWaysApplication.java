cba70efd87bb6c560bf9548bba7da0ac09ae9fe2
GeorgiStankov2025 <georgistankov05@gmail.com>
2026-04-04 14:15:12 +0300

    Initial commit

---
+++ src/main/java/com/georgistankov/gairways/GAirWaysApplication.java
@@ -0,0 +1,13 @@
+package com.georgistankov.gairways;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GAirWaysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GAirWaysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/georgistankov/gairways/GAirWaysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.georgistankov.gairways;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GAirWaysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
